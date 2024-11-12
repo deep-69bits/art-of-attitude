8d9cdd2104a88b494893d33cfc3b168c6cf3065e
deep-69bits <lcb2021004@iiitl.ac.in>
2024-11-12 07:32:02 -0800

    contact page Added

---
+++ app/contact/page.tsx
@@ -0,0 +1,62 @@
+"use client";
+import Container from "@/components/Container";
+import { Form } from "@/components/Home/FormSection";
+import Layout from "@/components/Layout/Layout";
+import Image from "next/image";
+import React from "react";
+
+const page = () => {
+  return (
+    <Layout>
+      <Container>
+        <div className="grid grid-flow-row lg:grid-cols-2 grid-cols-1 mt-20">
+          <div>
+            <div className="font leading-[5rem] text-6xl capitalize">
+              Get in touch <br /> with us & <br />{" "}
+              <span className="text-darkRed">
+                we will get <br /> you started
+              </span>
+            </div>
+
+            <div className="flex flex-col items-start mt-8 gap-4">
+              <div className="flex items-center gap-4">
+                <Image
+                  src="/contact/mail-fill.svg"
+                  width={40}
+                  height={40}
+                  alt="building fill"
+                />
+                <div className="text-2xl">gg@artofattitude.com</div>
+              </div>
+              <div className="flex items-center gap-8">
+                <Image
+                  src="/contact/mobile.svg"
+                  className="translate-x-2"
+                  width={20}
+                  height={30}
+                  alt="building fill"
+                />
+                <div className="text-2xl">+91 88604 26504</div>
+              </div>
+              <div className="flex items-center gap-4">
+                <Image
+                  src="/contact/building-fill.svg"
+                  width={40}
+                  height={40}
+                  alt="building fill"
+                />
+                <div className="text-2xl">
+                  Art Of Attitude Ventures LLP 1804,<br/> Sector 33 D Chandigarh,
+                  India
+                </div>
+              </div>
+            </div>
+          </div>
+          <Form />
+        </div>
+      </Container>
+    </Layout>
+  );
+};
+
+export default page;
--- components/Home/FormSection.js
+++ components/Home/FormSection.js
@@ -1,33 +1,72 @@
-import React from 'react'
-import Container from '../Container'
-import Image from 'next/image'
+import React from "react";
+import Container from "../Container";
+import Image from "next/image";
 
+export const Form = () => {
+  return (
+    <div className="flex flex-col justify-between">
+      <div className="capitalize text-6xl mb-10 font-light">
+        please fill the form and we will contact you
+      </div>
+      <input
+        placeholder="name"
+        type="text"
+        className="w-full mt-4  bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none "
+      />
+      <input
+        placeholder="company name"
+        type="text"
+        className="w-full mt-8 bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none "
+      />
+      <input
+        placeholder="email"
+        type="email"
+        className="w-full mt-8 bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none "
+      />
+      <input
+        placeholder="mobile number"
+        type="number"
+        className="w-full mt-8 bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none "
+      />
+      <div className="mt-8">
+        <label className="text-sm opacity-80">
+          Some details about your project
+        </label>
+        <textarea
+          rows={6}
+          placeholder="delivery date, goal of your project and other details "
+          className="w-full  bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none "
+        ></textarea>
+      </div>
+      <div className="mt-8 flex items-center gap-2">
+        <input type="checkbox" className="w-5 h-5 border-none"></input>
+        <div className="text-sm opacity-80">
+          I agree with terms of Privacy Policy
+        </div>
+      </div>
+      <button className="w-full  mt-8 border-none bg-darkRed py-3 text-center rounded-full font-semibold">
+        Get started today
+      </button>
+    </div>
+  );
+};
 const FormSection = () => {
   return (
-   <Container>
-    <div className='grid grid-flow-row lg:grid-cols-2 gap-10 grid-cols-1'>
-      <div>
-        <Image  alt='form image' className='w-[100%]' width={400} height={400} src={'/home/formImage.svg'}/>
-      </div>
-      <div className='flex flex-col justify-between'>
-        <div className='capitalize text-6xl mb-10 font-light'>please fill the form and we will contact you</div>
-        <input placeholder='name' type='text' className='w-full mt-4  bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none '/>
-        <input placeholder='company name' type='text' className='w-full mt-8 bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none '/>
-        <input placeholder='email' type='email' className='w-full mt-8 bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none '/>
-        <input placeholder='mobile number' type='number' className='w-full mt-8 bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none '/>
-        <div className='mt-8'>
-            <label className='text-sm opacity-80'>Some details about your project</label>
-            <textarea rows={6} placeholder='delivery date, goal of your project and other details ' className='w-full  bg-white border-none px-4 py-3 placeholder:text-[#7E7E7E] focus:outline-none '></textarea>
+    <Container>
+      <div className="grid grid-flow-row lg:grid-cols-2 gap-10 grid-cols-1">
+        <div>
+          <Image
+            alt="form image"
+            className="w-[100%]"
+            width={400}
+            height={400}
+            src={"/home/formImage.svg"}
+          />
         </div>
-        <div className='mt-8 flex items-center gap-2'>
-            <input type='checkbox' className='w-5 h-5 border-none'></input>
-            <div className='text-sm opacity-80'>I agree with terms of Privacy Policy</div>
-        </div>
-        <button className='w-full  mt-8 border-none bg-darkRed py-3 text-center rounded-full font-semibold'>Get started today</button>
+       <Form/>
       </div>
-    </div>
-   </Container>
-  )
-}
+    </Container>
+  );
+};
 
-export default FormSection
+export default FormSection;
